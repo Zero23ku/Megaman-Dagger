a5fdab73dbd1140cd448a8ddeab5dcc27ae306c6
Zero23ku <marcelo.cardenas@usach.cl>
2016-09-27 19:57:42 -0300

    Uploading Megaman Dagger
    -First demo level
    -Playable character
    -Left & Right movement
    -Collision detection (need get fixed)

---
+++ Assets/script/Zero/ZeroController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZeroController : MonoBehaviour {
+
+	public float maxS = 4f;
+	bool right = true;
+	Animator a;
+
+
+	// Use this for initialization
+	void Start () {
+        a = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    void Update(){
+        Movement();
+
+    }
+
+
+	void FixedUpdate () {
+        
+	}
+
+    void Flip()
+    {
+        right = !right;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    void Movement()
+    {
+        float move = Input.GetAxis("Horizontal");
+        a.SetFloat("speed", Mathf.Abs(move));
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxS, GetComponent<Rigidbody2D>().velocity.y);
+
+        //Left & Right movement//
+        if (move > 0 && !right)
+        {
+            Flip();
+        }
+        else
+        {
+            if (move < 0 && right)
+            {
+                Flip();
+            }
+        }
+
+        //Jump
+
+
+
+    }
+}
