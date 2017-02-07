bb92773f3c820bc307a37e49ba1311e2f94bde99
Zero23ku <marcelo.cardenas@usach.cl>
2017-02-07 16:22:08 -0300

    Bullet movement and Bullet destructor added
    The bullet only moves to the right and is destroyed when is outside of
    the camera.

--- Assets/Scripts/Megaman/MegamanBullet.cs
+++ Assets/Scripts/Megaman/MegamanBullet.cs
@@ -1,16 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
 
-public class MegamanBullet : MonoBehaviour {
+	public class MegamanBullet : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+		public float damage;
+
+		private float bulletSpeed = 9.5f;
+		private float spriteWidthDelta;
+		private Rigidbody2D bulletBody;
+		// Use this for initialization
+		void Start () {
+			spriteWidthDelta = GetComponent<SpriteRenderer>().bounds.size.x / 2;
+			bulletBody = GetComponent<Rigidbody2D>();
+		}
 		
+		// Update is called once per frame
+		void Update () {
+			BulletMovement();
+			//Destroy bullet if surpass right border.
+			if (transform.position.x + spriteWidthDelta > SpriteManager.maxPos.x) {
+				Destroy(gameObject);
+			}
+		}
+
+		void BulletMovement() {
+			bulletBody.velocity = new Vector2(bulletSpeed,bulletBody.velocity.y);
+			print(bulletBody.velocity);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
