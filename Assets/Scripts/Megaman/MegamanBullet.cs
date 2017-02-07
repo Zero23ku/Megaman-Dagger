7fde980ee342c218d761cd4be82446b319945aea
Zero23ku <marcelo.cardenas@usach.cl>
2017-02-07 17:00:25 -0300

    Added "player direction check" so the bullet moves to player's direction.
    Anyway, not working ATM

--- Assets/Scripts/Megaman/MegamanBullet.cs
+++ Assets/Scripts/Megaman/MegamanBullet.cs
@@ -9,23 +9,34 @@
 		private float bulletSpeed = 9.5f;
 		private float spriteWidthDelta;
 		private Rigidbody2D bulletBody;
+		private Transform playerTransform;
+		private Vector2 playerScale;
 		// Use this for initialization
 		void Start () {
 			spriteWidthDelta = GetComponent<SpriteRenderer>().bounds.size.x / 2;
 			bulletBody = GetComponent<Rigidbody2D>();
+			playerTransform = GameObject.FindWithTag("Player").transform;
 		}
 		
 		// Update is called once per frame
 		void Update () {
-			BulletMovement();
+			playerScale = playerTransform.localScale;
+			BulletMovement(playerScale.x);
 			//Destroy bullet if surpass right border.
 			if (transform.position.x + spriteWidthDelta > SpriteManager.maxPos.x) {
 				Destroy(gameObject);
 			}
+			
 		}
 
-		void BulletMovement() {
-			bulletBody.velocity = new Vector2(bulletSpeed,bulletBody.velocity.y);
-			print(bulletBody.velocity);
+		void BulletMovement(float direction) {
+			if (direction > 0)
+			{
+				bulletBody.velocity = new Vector2(bulletSpeed, bulletBody.velocity.y);
+			}
+			else { 
+				bulletBody.velocity = new Vector2(bulletSpeed*-1, bulletBody.velocity.y);
+			}
+			
 		}
 	}
