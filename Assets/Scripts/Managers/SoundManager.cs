089f96b7e73aaa969dfd2bbd13445330f7d2f6ad
Zero23ku <marcelo.cardenas@usach.cl>
2017-02-08 20:03:54 -0300

    Added bullet sound
    still need to work in SoundManager

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public static SoundManager instance = null;
+	public float lowPitchRange = 0.95f;
+	public float hihPitchRange = 1.05f;
+
+	private AudioSource sfxSource;
+	private AudioSource musicSource;
+	pivate float musicVolume;
+	private bool isFadeReady = false;
+	private bool soundPaused = false;
+
+
+	void Awake() {
+		if (!instance) {
+			instance = this;
+		}
+		else if (instance != this) {
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
