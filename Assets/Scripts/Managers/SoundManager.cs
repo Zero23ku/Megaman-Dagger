383c329967f3f667b7b9642b9af628005cde59b0
Zero23ku <marcelo.cardenas@usach.cl>
2017-02-09 15:26:17 -0300

    Added SoundManager
    NOW WITH :100: Music :100:  :+1: :+1:

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -6,12 +6,11 @@ public class SoundManager : MonoBehaviour {
 
 	public static SoundManager instance = null;
 	public float lowPitchRange = 0.95f;
-	public float hihPitchRange = 1.05f;
+	public float highPitchRange = 1.05f;
 
 	private AudioSource sfxSource;
 	private AudioSource musicSource;
-	pivate float musicVolume;
-	private bool isFadeReady = false;
+	private float musicVolume;
 	private bool soundPaused = false;
 
 
@@ -30,11 +29,38 @@ public class SoundManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		musicSource = GetComponents<AudioSource>()[0];
+		sfxSource = GetComponents<AudioSource>()[1];
+		musicVolume = musicSource.volume;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!soundPaused && GameManager.isPaused) {
+			soundPaused = true;
+			sfxSource.Pause();
+		}
+		else if (soundPaused && !GameManager.isPaused) {
+			soundPaused = false;
+			sfxSource.UnPause();
+			musicSource.UnPause();
+		}
+	}
+
+	public void PlaySingle(AudioClip clip) {
+		sfxSource.clip = clip;
+		sfxSource.Play();
+	}
+
+	public void RandomizeSFX(AudioClip clip) {
+		float randomPitch = Random.Range(lowPitchRange,highPitchRange);
+		sfxSource.pitch = randomPitch;
+		sfxSource.PlayOneShot(clip);
+	}
+
+	public void PlayMusic(AudioClip newClip) {
+		musicSource.clip = newClip;
+		musicSource.volume = musicVolume;
+		musicSource.Play();
 	}
 }
