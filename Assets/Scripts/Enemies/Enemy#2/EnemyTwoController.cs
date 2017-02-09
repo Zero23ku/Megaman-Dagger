8979a80fb6e5eaa59969ec24bf0317f36b6f5fc1
Zero23ku <marcelo.cardenas@usach.cl>
2017-02-09 17:22:22 -0300

    Enemy #2 prefab added, basic movement and interactions done

--- Assets/Scripts/Enemies/Enemy#2/EnemyTwoController.cs
+++ Assets/Scripts/Enemies/Enemy#2/EnemyTwoController.cs
@@ -4,13 +4,73 @@ using UnityEngine;
 
 public class EnemyTwoController : MonoBehaviour {
 
+	public float enemySpeed = 2.5f;
+	public int health;
+
+	//private GameObject player;
+	private Transform playerTransform;
+	private Transform selfTransform;
+	private Rigidbody2D selfBody;
+
+	private float spriteWitdhDelta;
+	private bool goingRight;
+
 	// Use this for initialization
 	void Start () {
+		playerTransform = GameObject.FindWithTag("Player").transform;
+		selfTransform = GetComponent<Transform>();
+		selfBody = GetComponent<Rigidbody2D>();
+		spriteWitdhDelta = GetComponent<SpriteRenderer>().bounds.size.x / 2;
+
+		//Know where is going the enemy for the first time.
+		if (selfTransform.localScale.x > 0) {
+			goingRight = false;
+		}
+
+		else {
+			goingRight = true;
+		}
+
+
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		ChangeDirectionMovement();
+		Movement();
+		print(selfTransform.position.x);
+	}
+
+	void Movement() {
+		if(!goingRight) {
+			selfBody.velocity = new Vector2(-enemySpeed,selfBody.velocity.y);
+		}
+		else {
+			selfBody.velocity = new Vector2(enemySpeed,selfBody.velocity.y);
+		}
+	}
+
+	void ChangeDirectionMovement() {
+		if (selfTransform.position.x < -6.24/*selfTransform.position.x + spriteWitdhDelta == SpriteManager.minPos.x + 100 && !goingRight*/) {
+			goingRight = true;
+		}
+		else if (selfTransform.position.x > 6.24/*selfTransform.position.x + spriteWitdhDelta == SpriteManager.maxPos.x - 25 && goingRight*/) {
+			goingRight = false;
+
+		}
+	
+	}
+
+	public void receiveDamage() {
+		health--;
+		if (health <= 0)
+		{
+			Die();
+		}
+	}
+
+	void Die() {
+		Destroy(gameObject);
 	}
 }
