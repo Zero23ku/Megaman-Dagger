fd95f76ad4bef0b4821e2867a72ff98c7dc58da1
Zero23ku <marcelo.cardenas@usach.cl>
2017-02-21 18:38:15 -0300

    fix'd triggerbox and colliderbox

---
+++ Assets/Scripts/Enemies/Enemy#3/enemyThreeTriggerBoxScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyThreeTriggerBoxScript : MonoBehaviour {
+
+	private enemyDiskFloorController enemyDiskFloorController;
+
+	// Use this for initialization
+	void Start () {
+		enemyDiskFloorController = GetComponent<enemyDiskFloorController>();
+	}
+	
+
+	void OnTriggerEnter2D(Collider2D otherCollider) {
+
+		if (otherCollider.tag == "triggerBox"){
+			print("IZQUIERDA");
+		}
+		else if (otherCollider.tag == "edgeBox")
+		{
+			enemyDiskFloorController.enemySpeed *= -1;
+
+
+		}
+		else if (otherCollider.tag == "edgeBox")
+		{
+			enemyDiskFloorController.enemySpeed *= -1;
+			print("DERECHA");
+		}
+	}
+
+}
