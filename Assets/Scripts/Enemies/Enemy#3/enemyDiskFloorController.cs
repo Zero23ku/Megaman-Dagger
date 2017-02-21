4a2f8fdeb18d91f44e51395b023401ec8d452fff
Zero23ku <marcelo.cardenas@usach.cl>
2017-02-21 17:59:16 -0300

    Added new enemy

---
+++ Assets/Scripts/Enemies/Enemy#3/enemyDiskFloorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyDiskFloorController : MonoBehaviour {
+
+	public int healt;
+	public float enemySpeed = 3.0f;
+
+	public Collider2D leftLimit;
+	public Collider2D rightLimit;
+	public Rigidbody2D selfBody;
+	public GameObject platform;
+
+
+	// Use this for initialization
+	void Start () {
+		selfBody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement();	
+	}
+
+
+	void Movement() {
+
+		selfBody.velocity = new Vector2(enemySpeed,selfBody.velocity.y);
+	
+	}
+}
---
+++ Assets/Scripts/Enemies/Enemy#3/enemyThreeHitBoxScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyThreeHitBoxScript : MonoBehaviour {
+
+	private enemyDiskFloorController enemyDiskFloorController;
+
+	// Use this for initialization
+	void Start () {
+		enemyDiskFloorController = GetComponent<enemyDiskFloorController>();
+	}
+	
+
+	void OnTriggerEnter2D(Collider2D otherCollider) {
+		if (otherCollider.tag == "triggerBox")
+		{
+			enemyDiskFloorController.platform = otherCollider.GetComponentInParent<GameObject>();
+			enemyDiskFloorController.leftLimit = enemyDiskFloorController.platform.GetComponentsInChildren<Collider2D>()[0];
+			enemyDiskFloorController.rightLimit = enemyDiskFloorController.platform.GetComponentsInChildren<Collider2D>()[1];
+			print(otherCollider.tag);
+		}
+		else if (otherCollider == enemyDiskFloorController.leftLimit)
+		{
+			enemyDiskFloorController.enemySpeed *= -1;
+			print("IZQUIERDA");
+
+		}
+		else if (otherCollider == enemyDiskFloorController.rightLimit)
+		{
+			enemyDiskFloorController.enemySpeed *= -1;
+			print("DERECHA");
+		}
+	}
+
+}
