5f5ca7ed4cf1340fe0518eec58b5b54e86eca837
Zero23ku <marcelo.cardenas@usach.cl>
2017-03-07 16:43:36 -0300

    added items
    health item and invulnerability item, btw, they don't drop

---
+++ Assets/ItemScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour {
+
+	public bool giveInvulnerability;
+	public int giveHealth;
+
+	public bool isUsed = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isUsed) {
+			Destroy(gameObject);
+		}
+
+	}
+}
