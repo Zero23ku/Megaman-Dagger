85a2252d74b60370a4ddbc928ca42134cab0dbad
Zero23ku <marcelo.cardenas@usach.cl>
2017-03-08 17:50:05 -0300

    Added death animation
    is shit btw

--- Assets/MegamanDeathController.cs
+++ Assets/MegamanDeathController.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class MegamanDeathController : MonoBehaviour {
 
     public float selfSpeed = 1.7f;
-    public string direction = "down";
+    public string direction;
 
     private Rigidbody2D selfBody;
 
@@ -14,14 +15,19 @@ public class MegamanDeathController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         selfBody = GetComponent<Rigidbody2D>();
-        if (direction != "up" || direction != "down") {
+        if (direction == "left" || direction == "right") {
             selfBody.constraints = RigidbodyConstraints2D.FreezePositionY;
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        print(direction);
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName == "Main Menu" || sceneName == "Game Over")
+            Destroy(gameObject);
         Movement(direction);
+        
 	}
 
 
